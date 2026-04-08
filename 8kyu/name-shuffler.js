cc651c71c4c261f51be4ca6b5db89a98fc6081e9
Hackasnack <101485120+Hackasnack@users.noreply.github.com>
2026-04-08 13:41:19 -0700

    Name Shuffler

---
+++ 8kyu/name-shuffler.js
@@ -0,0 +1,14 @@
+// Name Shuffler
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// Example(Input --> Output)
+
+// "john McClane" --> "McClane john"
+
+// my code
+
+function nameShuffler(str){
+  let names = str.split(' ')
+  return `${names[1]} ${names[0]}`
+}
