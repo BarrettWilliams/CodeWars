523fd8faded3f8f4b984f5b711c863fda508f27e
Hackasnack <101485120+Hackasnack@users.noreply.github.com>
2026-04-24 00:07:22 -0700

    Function 2 - squaring an argument

---
+++ 8kyu/squaring-an-argument.js
@@ -0,0 +1,9 @@
+// Function 2 - squaring an argument
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// my code 
+
+function square(n){
+  return n * n
+}
