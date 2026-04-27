3a978f695e63ac5adaae25248b9125cde71ef9a2
Hackasnack <101485120+Hackasnack@users.noreply.github.com>
2026-04-27 01:54:38 -0700

    Super Duper Easy

---
+++ 8kyu/super-duper-easy.js
@@ -0,0 +1,14 @@
+// Super Duper Easy
+
+
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+// my code 
+
+function problem(x){
+  if(x === Number(x)){
+    return x *50 +6
+  }else {
+    return 'Error'
+  }
+}
