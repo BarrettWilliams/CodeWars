ea746a350498e9671584f5f4316486943513c861
Barrett Williams <101485120+BarrettWilliams@users.noreply.github.com>
2026-06-03 22:58:51 -0700

    Remove all exclamation marks from the end

---
+++ 8kyu/remove-exclamationmarks-from-end.js
@@ -0,0 +1,18 @@
+// Exclamation marks series #2: Remove all exclamation marks from the end of sentence
+
+// Description:
+// Remove all exclamation marks from the end of sentence.
+
+// Examples
+// "Hi!"     ---> "Hi"
+// "Hi!!!"   ---> "Hi"
+// "!Hi"     ---> "!Hi"
+// "!Hi!"    ---> "!Hi"
+// "Hi! Hi!" ---> "Hi! Hi"
+// "Hi"      ---> "Hi"
+
+// my code 
+
+function remove(string) {  
+   return string.replace(/!+$/, "")
+}
