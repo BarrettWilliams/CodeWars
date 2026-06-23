1208f7628bd18018e8c47fda7ebffd0e8c8aea3a
Barrett Williams <101485120+BarrettWilliams@users.noreply.github.com>
2026-06-23 12:37:57 -0700

    Remove the time

---
+++ 8kyu/remove-the-time.js
@@ -0,0 +1,22 @@
+// Remove the time
+
+// Task Description
+// You're re-designing a blog, and the blog's posts have the Weekday Month Day, time format for showing the date and time when a post was made, e.g., Friday May 2, 7pm.
+
+// You're running out of screen real estate, and on some pages you want to display a shorter format, Weekday Month Day that omits the time.
+
+// Write a function that takes the website date/time in its original string format and returns the shortened format.
+
+// Input
+// Input will always be a string, e.g., "Friday May 2, 7pm". 
+
+// Output
+// Output will be the shortened string, e.g., "Friday May 2".
+
+// my code 
+
+function shortenToDate(longDate) {
+  let arr = longDate.split(', ')
+  let kick = arr.pop()
+  return arr.join()
+}
