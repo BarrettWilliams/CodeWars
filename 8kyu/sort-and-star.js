2a86e829f4f6c7cdce6f444071c4687d54b6b76f
Barrett Williams <101485120+BarrettWilliams@users.noreply.github.com>
2026-07-02 13:19:45 -0700

    Sort and Star

---
+++ 8kyu/sort-and-star.js
@@ -0,0 +1,16 @@
+// Sort and Star
+
+// You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+// The returned value must be a string, and have "***" between each of its letters.
+
+// You should not remove or add elements from/to the array.
+
+// my code 
+
+function twoSort(s) {
+  let a = s.sort()
+  let first = a[0]
+  return first.split('').join('***')
+
+}
