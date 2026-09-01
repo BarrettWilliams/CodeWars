53bd8d07f25916080340ad91041a24490a1740b2
Hackasnack <aheadofyou2@gmail.com>
2026-09-01 16:25:02 -0700

    Parse float

---
+++ 8kyu/parse-float.js
@@ -0,0 +1,9 @@
+// Parse float
+
+// Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
+
+// my code 
+
+function parseF(s) {
+  return parseFloat(s) === Number(s)? parseFloat(s) : null;
+}
