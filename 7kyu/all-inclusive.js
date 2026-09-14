78ecd73bc208e81602d3f0dc2ce462962e647988
Hackasnack <aheadofyou2@gmail.com>
2026-09-13 23:58:19 -0700

    All Inclusive?

---
+++ 7kyu/all-inclusive.js
@@ -0,0 +1,46 @@
+// All Inclusive?
+
+
+// Input:
+
+// a string strng
+// an array of strings arr
+// Output of function contain_all_rots(strng, arr) (or containAllRots or contain-all-rots):
+
+// a boolean true if all rotations of strng are included in arr
+// false otherwise
+// Examples:
+// contain_all_rots(
+//   "bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]) -> true
+
+// contain_all_rots(
+//   "Ajylvpy", ["Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee"]) -> false)
+// Note:
+// Though not correct in a mathematical sense
+
+// we will consider that there are no rotations of strng == ""
+// and for any array arr: contain_all_rots("", arr) --> true
+
+// my code 
+
+function containAllRots(strng, arr) {
+  
+  if(strng === ''){
+    return true
+  }
+  
+    let timey = strng.length
+    let huh = []
+    
+    let x = strng.split('')
+    
+    for(let i = 0; i < timey; i++){
+      let shif = x.shift()
+      x.push(shif)
+      
+      huh.push(x.join(''))
+    }
+  
+  return huh.every(rotation => arr.includes(rotation))
+   
+}
