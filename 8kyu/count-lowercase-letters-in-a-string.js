a160125c6e0e68bd071af69369cfce2ba236158e
Hackasnack <aheadofyou2@gmail.com>
2026-09-18 23:22:09 -0700

    count lowercase letters in a string

---
+++ 8kyu/count-lowercase-letters-in-a-string.js
@@ -0,0 +1,24 @@
+// count lowercase letters in a string
+
+// Your task is simply to count the total number of lowercase letters in a string.
+
+// Examples
+// "abc" ===> 3
+
+// "abcABC123" ===> 3
+
+// "abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 3
+
+// "" ===> 0;
+
+// "ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 0
+
+// "abcdefghijklmnopqrstuvwxyz" ===> 26
+
+// my code 
+
+function lowercaseCount(str){
+   const matches = str.match(/[a-z]/g);
+  console.log(matches)
+  return matches? matches.length : 0
+}
